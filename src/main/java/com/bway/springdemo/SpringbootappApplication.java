a328b3d58ed1f09f26f178187860695feab44381
THESHREEK <shreekishna.thapamagar@gmail.com>
2023-01-22 16:42:47 +0545

    initial commit

---
+++ src/main/java/com/bway/springdemo/SpringbootappApplication.java
@@ -0,0 +1,13 @@
+package com.bway.springdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bway/springdemo/SpringbootappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bway.springdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
